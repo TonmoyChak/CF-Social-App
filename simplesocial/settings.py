dac43802aa7c5bfbfb436ab7d9ccfba901c0556a
Tonmoy <59925709+TonmoyChak@users.noreply.github.com>
2021-07-24 01:25:26 -0400

    Update settings.py

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--gy-=wvpyz^btn3sm_p@g%5s00vx#dwl)g!^-&v^^e=uya2vc4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
